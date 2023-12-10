019f62a293d428f8d57858b15c44ba572d4dd125
Esther Adejola <adejolanu@gmail.com>
2023-12-10 22:15:08 +0100

    Added files

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 var data = [
   {
     id: 1,
-    listImg: "../Images/Rectangle 77.png",
+    listImg: "../Images/Rectangle 77.jpg",
     country: "JAPAN",
     linkToGoogle:
       "https://www.google.com/maps/place/Mount+Fuji/@35.3606233,138.7067638,14z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu",
@@ -12,7 +12,7 @@ var data = [
   },
   {
     id: 2,
-    listImg: "../Images/Rectangle 84.png",
+    listImg: "../Images/Rectangle 84.jpg",
     country: "AUSTRALIA",
     linkToGoogle:
       "https://www.google.com/maps/place/Sydney+Opera+House/@-33.8567844,151.2152967,17z/data=!3m1!4b1!4m6!3m5!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967!16zL20vMDZfbm0?entry=ttu",
@@ -23,7 +23,7 @@ var data = [
   },
   {
     id: 3,
-    listImg: "../Images/Rectangle 78.png",
+    listImg: "../Images/Rectangle 78.jpg",
     country: "NORWAY",
     linkToGoogle:
       "https://www.google.com/maps/place/Geirangerfjord/@62.1048487,7.0746377,11z/data=!3m1!4b1!4m6!3m5!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940817!16zL20vMDZ4eXFi?entry=ttu",
